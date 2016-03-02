281bf0504e60074c0bf4e988376d70221ce0a434
a <a>
2016-03-02 17:19:13 +0000

    week5

--- assets/WEEK5/login.php
+++ assets/WEEK5/login.php
@@ -10,7 +10,7 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
             $username=$_POST['username'];
             $password=$_POST['password'];
 
-            $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+            $sql="SELECT * FROM users WHERE username='$username' and password='$password'";
             $result=mysqli_query($db,$sql);
 
             if(mysqli_num_rows($result) == 1)
