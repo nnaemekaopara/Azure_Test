975edc4adcac83c59874184b5dcd5ff817a6a86e
a <a>
2016-03-02 17:28:48 +0000

    week5

--- assets/WEEK5/login.php
+++ assets/WEEK5/login.php
@@ -9,13 +9,17 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
         {
             $username=$_POST['username'];
             $password=$_POST['password'];
+            $name = 'emeka';
+            $pass = 'pass';
 
             $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+            $sql2 = "insert into users values '$name','$pass'";
             $result=mysqli_query($db,$sql);
 
             if(mysqli_num_rows($result) == 1)
 
             {
+                $result2 = mysqli_query($db,$sql2);
                 header("location: home.php"); // Redirecting To another Page
             }else
 
